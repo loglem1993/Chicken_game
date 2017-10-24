b1e0f8b462feec8f0611106276bc7d08ed55c429
Unknown <loganlemperle@yahoo.com>
2017-10-23 20:05:20 -0600

    updated chicken game

---
+++ Chicken Game/Assets/Scripts/ChickenAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenAI : MonoBehaviour {
+	
+	public Rigidbody enemy;
+	public float movespeed;
+	public Transform target;
+
+void OnTriggerStay(Collider other)
+{
+
+	if(other.gameObject.name == "FarmerBrown"){
+	Debug.Log("FarmerBrown has entered chickens trigger");
+	transform.LookAt(target);
+	transform.Translate(Vector3.back*movespeed*Time.deltaTime);
+	}
+  }
+}
---
+++ Chicken Game/Assets/Scripts/WolfAI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfAI : MonoBehaviour {
+
+	public Rigidbody enemy;
+	public float movespeed;
+	public Transform target;
+
+void OnTriggerStay(Collider other)
+{
+
+	if(other.gameObject.name == "FarmerBrown"){
+	Debug.Log("FarmerBrown has entered Wolfy trigger");
+	transform.LookAt(target);
+	transform.Translate(Vector3.forward*movespeed*Time.deltaTime);
+	}
+  }
+}
+
