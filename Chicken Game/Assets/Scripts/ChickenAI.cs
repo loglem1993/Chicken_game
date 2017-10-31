d434405ada1cfd3a910b4c02aca642ddc4ffcb45
Unknown <loganlemperle@yahoo.com>
2017-10-30 20:22:59 -0600

    New files

--- Chicken Game/Assets/Scripts/ChickenAI.cs
+++ Chicken Game/Assets/Scripts/ChickenAI.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class ChickenAI : MonoBehaviour {
 	
-	public Rigidbody enemy;
-	public float movespeed;
-	public Transform target;
+	public float moveSpeed;
+
+	public Transform FarmerBrown;
+
+	public Transform chickenPen;
+
+	public Transform Wolfy;
+	public int points = 10; 
 
 void OnTriggerStay(Collider other)
 {
 
-	if(other.gameObject.name == "FarmerBrown"){
-	Debug.Log("FarmerBrown has entered chickens trigger");
-	transform.LookAt(target);
-	transform.Translate(Vector3.back*movespeed*Time.deltaTime);
+	if(other.gameObject.name == "FarmerBrown")
+	{
+	Debug.Log("FarmerBrown has entered wolfs trigger");
+	transform.LookAt(FarmerBrown);
+	transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
+	}
+	else if(other.gameObject.name =="Wolfy")
+	{
+	Debug.Log("Wolf has entered Chicken's trigger");
+	transform.LookAt(Wolfy);
+	transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
+	}
+}
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.name == "Player"){
+			scoreManager.Addpoints(points);
+			transform.position = chickenPen.position;
+			transform.rotation = chickenPen.rotation;
+		
+		
+	
 	}
   }
 }
+
