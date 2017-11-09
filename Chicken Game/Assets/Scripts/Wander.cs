b022f13928548d7c57702220b09faf9f36c2477e
Unknown <loganlemperle@yahoo.com>
2017-11-08 20:06:13 -0700

    chicken game

---
+++ Chicken Game/Assets/Scripts/Wander.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour {
+
+public float moveSpeed;
+
+void MoveForward(){
+	transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+
+}
+ 
+void Turn(){
+	int randomNum = Random.Range(0,360);
+	transform.Rotate(0,randomNum,0);
+}
+
+void OnTriggerStay(Collider other){
+	if(other.gameObject.tag == "Checkpoint")
+	Turn();
+else {
+	MoveForward();
+}
+
+
+	
+}
+
+
+}
